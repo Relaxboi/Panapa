3da6a7ae2976b771baed5a249fdd58da20d456bb
Rober19 <texas_telecaster@hotmail.com>
2017-11-07 14:19:33 -0500

    otro commit

---
+++ src/Controller/PanController.java
@@ -0,0 +1,24 @@
+package Controller;
+
+import Model.Pan;
+import java.util.ArrayList;
+
+public class PanController {
+
+    ArrayList<Pan> ListaPan = new ArrayList();
+    
+    public void Create() {
+    Pan pan1 = new Pan(); 
+    }
+
+    public void Read() {
+    
+    }
+
+    public void Uptade() {
+    }
+
+    public void Delete() {
+    }
+
+}
---
+++ src/Model/Pan.java
@@ -0,0 +1,50 @@
+package Model;
+
+public class Pan {
+
+    private String nombreTipo;
+    private int precio;
+    private int cantidad;
+    private String id;
+
+    public Pan(String nombreTipo, int precio, int cantidad, String id){
+        this.nombreTipo = nombreTipo;
+        this.precio = precio;
+    }
+    public Pan(){}
+
+    public String getNombreTipo() {
+        return nombreTipo;
+    }
+
+    public void setNombreTipo(String nombreTipo) {
+        this.nombreTipo = nombreTipo;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    
+
+}
