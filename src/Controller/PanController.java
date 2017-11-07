bbefaf0b2a9940aac9bbbf0ca04917915f0cf0f9
Anomium <32109574+Anomium@users.noreply.github.com>
2017-11-07 16:08:30 -0500

    Cambios
    asd

--- src/Controller/PanController.java
+++ src/Controller/PanController.java
@@ -7,18 +7,18 @@ public class PanController {
 
     ArrayList<Pan> ListaPan = new ArrayList();
     
-    public void Create(Pan Pan1) {
+    public void Create(Pan Pan1) {//Crea un tipo de pan - datos en cola
         ListaPan.add(Pan1);
     }
 
-    public void Read() {
+    public void Read() {//Busca leer/buscar
 
     }
 
-    public void Uptade() {
+    public void Uptade() {//Actualizar o modificar
     }
 
-    public void Delete() {
+    public void Delete() {//Eliminar
     }
 
 }
