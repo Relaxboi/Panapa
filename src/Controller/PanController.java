5dde0ad755e332c430a7c246d91f951ed9395bfa
FernanGamerFlo <33295357+FernanGamerFlo@users.noreply.github.com>
2017-11-08 13:19:38 -0500

    fdag

--- src/Controller/PanController.java
+++ src/Controller/PanController.java
@@ -12,26 +12,7 @@ public class PanController {
     }
 
     public void Read(String s) {//Busca leer/buscar
-        String Palabra = "";
-        for (int i = 0; i < ListaPan.size(); i++) {
-            Palabra = "";
-            for (int b = 0; b < ListaPan.get(i).getNombreTipo().length(); b++) {
-                if (ListaPan.get(i).getNombreTipo().charAt(b) != ' ') {
-                    Palabra += ListaPan.get(i).getNombreTipo().charAt(b);
-                    if (Palabra.equals(s)) {
-                        System.out.println(ListaPan.get(i).getNombreTipo());
-                        break;
-                    }
-                
-                }else{
-                    Palabra = "";
-                }
-            }
-
-        }
-        for(int i = 0; i < 100; i++){
-            System.out.println();
-        }
+        
 
     }
 
