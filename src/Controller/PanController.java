906ec165da5992407200f07a939fbef75ce29488
Rober19 <texas_telecaster@hotmail.com>
2017-11-08 11:47:54 -0500

    Primera Tabulacion

--- src/Controller/PanController.java
+++ src/Controller/PanController.java
@@ -6,7 +6,8 @@ import java.util.ArrayList;
 public class PanController {
 
     ArrayList<Pan> ListaPan = new ArrayList();
-    
+    Object[][] ListaPanesMtz = new Object[getListaPan().size()][3];
+
     public void Create(Pan Pan1) {//Crea un tipo de pan - datos en cola
         ListaPan.add(Pan1);
     }
@@ -15,12 +16,24 @@ public class PanController {
 
     }
 
-    public void Uptade() {//Actualizar o modificar
+    public void Uptade(int index) {//Actualizar o modificar
+
     }
 
     public void Delete() {//Eliminar
     }
-    
+
+    public void Listar() {
+        ListaPanesMtz = new Object[getListaPan().size()][3];
+        
+        for (int i = 0; i < getListaPan().size(); i++) {
+            ListaPanesMtz[i][0] = getListaPan().get(i).getNombreTipo();
+            ListaPanesMtz[i][1] = getListaPan().get(i).getCantidad();
+            ListaPanesMtz[i][2] = getListaPan().get(i).getPrecio();
+        }
+
+    }
+
     //Set and get del ArrayList
     public ArrayList<Pan> getListaPan() {
         return ListaPan;
@@ -29,6 +42,15 @@ public class PanController {
     public void setListaPan(ArrayList<Pan> ListaPan) {
         this.ListaPan = ListaPan;
     }
+
+    public Object[][] getListaPanesMtz() {
+        return ListaPanesMtz;
+    }
+
+    public void setListaPanesMtz(Object[][] ListaPanesMtz) {
+        this.ListaPanesMtz = ListaPanesMtz;
+    }
     
     
+
 }
